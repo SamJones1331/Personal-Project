106df4a5fe1ce5ed6b5356d9667c3ec17a9f062b
SamJones1331 <samuel.jones@academytrainee.com>
2019-11-07 16:44:17 +0000

    edited the startup.sh file to install to be up to date with the new method of running the app

--- startup.sh
+++ startup.sh
@@ -1,18 +1,19 @@
-cd ~
-cd /home/fezgaming/Personal-Project/application
-. ./venv/bin/activate
-sudo apt-get install python3-pip
-sudo apt-get install python3-venv
-pip3 install pyopenssl
-
-export FLASK_APP=run.py
-export FLASK_ENV=production
-export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=5000
-export FLASK_RUN_CERT=cert.pem
-export FLASK_RUN_KEY=key.pem
-pip3 install flask-sqlalchemy
-pip3 install flask-bcrypt
-pip3 install flask-login
-pip3 install flask-wtf
-pip3 install wtforms-sqlalchemy
+sudo apt-get update
+#curls the cript from a webpage and then passes it through bash
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker $(whoami)
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "Preparing to log you out"
+sleep 1
+echo "Remember to log back in once the terminal closes"
+sleep 1
+echo "Logging you out in 3"
+sleep 1
+echo "Logging you out in 2"
+sleep 1
+echo "Logging you out in 1"
+sleep 1
+echo "Logging you out"
+logout
