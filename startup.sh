84833d402a3075e941b26a4c6e2a6e12c22b0204
SamJones1331 <samuel.jones@academytrainee.com>
2019-11-08 11:03:29 +0000

    Ammended the stratup.sh script and added a second one

--- startup.sh
+++ startup.sh
@@ -2,9 +2,6 @@ sudo apt-get update
 #curls the cript from a webpage and then passes it through bash
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
-# install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 echo "Preparing to log you out"
 sleep 1
 echo "Remember to log back in once the terminal closes"
